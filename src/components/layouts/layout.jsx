0ecf81a021b258aa9426a9986e35c426be67cd16
Marcelo1Marques <marcelo99david@gmail.com>
2022-11-15 16:50:23 +0100

    added content and images after introduction

--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -1,7 +1,15 @@
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
 import Image from "next/image";
-import Profile from "../../assets/img/proxy.jpg"
+import Profile from "../../assets/img/proxy.jpg";
+import Html from "../../assets/img/html5.png";
+import Css from "../../assets/img/css.png";
+import Javascript from "../../assets/img/javascript.png";
+import ReactImg from "../../assets/img/react.png";
+import Sql from "../../assets/img/sql.png";
+import Php from "../../assets/img/php.png";
+import Nodejs from "../../assets/img/nodejs.png";
+import Python from "../../assets/img/python.png";
 import React from "react";
 import styles from "@src/components/layouts/styles";
 import { useSpring, animated } from 'react-spring';
@@ -24,6 +32,22 @@ const Layout = ({children}) => {
                         </div>
                         <h1 className={styles.title}>Qui suis je?</h1>
                         <p className={styles.paragraph}>Je m&apos;appelle Marcelo et bienvenue dans mon portfolio. Vous pouvez lire et voir mon travail en tant que développeur web au fil des ans. Pour toutes questions, n&apos;hésitez pas à me contacter via mon espace contact. <br /> Merci de votre visite.</p>
+                        <h1 className={styles.title}>Domaines de travail</h1>
+                        <h3 className={styles.h3}>Avec quoi je travaille ?</h3>
+                        <h3 className={styles.h3}>Front-End</h3>
+                        <span className={styles.frontend}>
+                            <Image src={Html} alt="logo" width="150" height="150" />
+                            <Image src={Css} alt="logo" width="170" height="150" />
+                            <Image src={Javascript} alt="logo" width="150" height="150" />
+                            <Image src={ReactImg} alt="logo" width="150" height="150" />
+                        </span>
+                            <h3 className={styles.h3}>Back-End</h3>
+                        <span className={styles.backend}>
+                            <Image src={Sql} alt="logo" width="160" height="150" />
+                            <Image src={Php} alt="logo" width="170" height="150" />
+                            <Image src={Nodejs} alt="logo" width="150" height="150" />
+                            <Image src={Python} alt="logo" width="150" height="150" />
+                        </span>
                     </section>
                     <hr className={styles.hr} />
                     <section className={styles.section}>
